d37acf6fc13a773a6e10e96a654a3ad3c38bca7c
Lucas Bandermann <lucasbandermann@gmail.com>
2021-04-19 02:20:49 +0200

    Added LucraftCore

--- src/mods.js
+++ src/mods.js
@@ -10,5 +10,11 @@ module.exports.mods = [
         id: 316946,
         color: 0x7e0707,
         thumbnail: 'https://media.forgecdn.net/avatars/thumbnails/195/102/64/64/636876780539263471.png'
+    },
+    {
+        name: 'LucraftCore',
+        id: 230651,
+        color: 0xaa9889,
+        thumbnail: 'https://media.forgecdn.net/avatars/84/792/636197981579397621.png'
     }
 ]
